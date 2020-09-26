6b429b8b5edb7773f7782fa3f6f2a4a9c4cc415a
GARACHBHUMI <36135895+GARACHBHUMI@users.noreply.github.com>
2020-09-26 18:15:37 +0530

    first commit

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by ravi on 08/09/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct CoinData:Decodable {
+    var rate:Double
+}
